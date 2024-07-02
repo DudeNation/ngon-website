6429deac1970d419a28523911eb6ea178219f3b1
Sol <93467735+tinryan2003@users.noreply.github.com>
2024-07-02 10:51:03 +0700

    Update

--- ngon Admin/ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	  name: "NGON-CLIENT",
-	  script: "serve",
-	  env: {
-		      PM2_SERVE_PATH: "./build",
-		      PM2_SERVE_PORT: 1000,
-		      PM2_SERVE_SPA: "true",
-		    },
-};
--- ngon Admin/src/redux/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import cartReducer from './reducer/cartReducer';
-
-const rootReducer = combineReducers({
-  cart: cartReducer,
-});
-
-export default rootReducer;
